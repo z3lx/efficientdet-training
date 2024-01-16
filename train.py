a68790f294252704a005ca338937ab08c2347262
z3lx <57017122+z3lx@users.noreply.github.com>
2024-01-16 18:43:40 -0500

    Add logging dir to training script

--- train.py
+++ train.py
@@ -10,11 +10,18 @@ tf.get_logger().setLevel('INFO')
 from absl import logging
 logging.set_verbosity(logging.INFO)
 
-# Set model architecture
-spec = model_spec.get('efficientdet_lite0')
+#print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+#tf.debugging.set_log_device_placement(True)
 
 venv_dir = os.environ['VIRTUAL_ENV']
 os.chdir(venv_dir + '/..')
+
+# Set model architecture
+spec = model_spec.get(
+    'efficientdet_lite0',
+    model_dir = 'runs/run_0',
+    tf_random_seed = 111111
+)
 
 # Load dataset
 train_data = object_detector.DataLoader.from_pascal_voc(
@@ -27,14 +34,18 @@ train_data = object_detector.DataLoader.from_pascal_voc(
 model = object_detector.create(
     train_data = train_data,
     model_spec = spec,
-    batch_size = 16,
+    batch_size = 32,
     train_whole_model = True,
     validation_data = train_data,
-    epochs = 50
+    epochs = 2
 )
 
 # Export model
 model.export(
     export_dir = 'export',
-    export_format = [ExportFormat.SAVED_MODEL, ExportFormat.LABEL]
+    export_format = [
+        ExportFormat.TFLITE,
+        ExportFormat.SAVED_MODEL,
+        ExportFormat.LABEL
+    ]
 )
