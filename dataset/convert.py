afe339124f37d26f1fcdabfb89800fec84e0f7a0
z3lx <57017122+z3lx@users.noreply.github.com>
2024-01-16 16:32:29 -0500

    Fix labels conversion

--- dataset/convert.py
+++ dataset/convert.py
@@ -6,22 +6,57 @@ import shutil
 venv_dir = os.environ['VIRTUAL_ENV']
 os.chdir(venv_dir + '/../dataset')
 
-images_dir = './yolo/images'
-labels_dir = './yolo/labels'
-images_output_dir = './voc/images'
-labels_output_dir = './voc/labels'
+images_dir = 'yolo/images'
+labels_dir = 'yolo/labels'
+images_output_dir = 'voc/images'
+labels_output_dir = 'voc/labels'
 
+# Checks
+if not os.path.isdir(images_dir):
+    raise Exception(f'Directory {images_dir} does not exist.')
+if not os.path.isdir(labels_dir):
+    raise Exception(f'Directory {labels_dir} does not exist.')
+
+# Cleanup
+shutil.rmtree(images_output_dir, ignore_errors=True)
+shutil.rmtree(labels_output_dir, ignore_errors=True)
+os.makedirs(images_output_dir, exist_ok=True)
+os.makedirs(labels_output_dir, exist_ok=True)
+
+# Convert images
 for filename in os.listdir(images_dir):
-    if filename.endswith(".png") or filename.endswith(".jpg"):
+    if filename.endswith('.png') or filename.endswith('.jpg'):
         img = Image.open(os.path.join(images_dir, filename))
         img = img.convert('RGB')
         img.save(os.path.join(images_output_dir, filename.split('.')[0] + '.jpeg'), 'JPEG', quality=100)
     else:
-        if filename.filename.endswith(".jpeg"):
+        if filename.filename.endswith('.jpeg'):
             shutil.copy(os.path.join(images_dir, filename), images_output_dir)
 
+# Convert labels
 Convertor.yolo2voc(
-    dataset_dir=os.path.join(images_dir, '..'),
-    yolo_ann_dir=os.path.join(labels_dir, '..'),
+    dataset_dir = images_dir + '/..',
+    yolo_ann_dir = labels_dir + '/..',
     save_dir=labels_output_dir
 )
+
+# Fix labels
+import xml.etree.ElementTree as ET
+for filename in os.listdir(labels_output_dir):
+    if (filename.endswith('.xml')):
+        filepath = os.path.join(labels_output_dir, filename)
+        tree = ET.parse(filepath)
+        root = tree.getroot()
+
+        filename_tag = root.find('filename')
+        filename_tag.text = filename_tag.text.replace('.png', '.jpeg')
+        root.find('path').text = filename_tag.text
+        for name_elem in root.iter('name'):
+            name_elem.text = 'note'
+            #try:
+            #    double_value = float(name_elem.text)
+            #    name_elem.text = str(int(double_value))
+            #except ValueError:
+            #    pass
+
+        tree.write(filepath)
--- dataset/sample.py
+++ dataset/sample.py
@@ -5,10 +5,10 @@ venv_dir = os.environ['VIRTUAL_ENV']
 os.chdir(venv_dir + '/../dataset')
 
 Sample.show_samples(
-    data_path='./yolo/images',
-    ann_path='./voc/labels',
-    num_of_samples=5,
-    ann_type='voc',
-    seed=0,
-    image_shape=[512, 512]
+    data_path = 'voc/images',
+    ann_path = 'voc/labels',
+    num_of_samples = 5,
+    ann_type = 'voc',
+    seed = 0,
+    image_shape = [512, 512]
 )
