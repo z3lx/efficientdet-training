ab78ab06677a070a2d427293924508c66175a244
z3lx <57017122+z3lx@users.noreply.github.com>
2024-01-16 14:54:57 -0500

    Change format to VOC

--- dataset/convert.py
+++ dataset/convert.py
@@ -1,9 +1,27 @@
 from imgann import Convertor
+from PIL import Image
 import os
+import shutil
 
 venv_dir = os.environ['VIRTUAL_ENV']
 os.chdir(venv_dir + '/../dataset')
 
-Convertor.yolo2csv(dataset_dir='./yolo/',
-                   yolo_ann_dir='./yolo/',
-                   save_dir='./csv/dataset.csv')
+images_dir = './yolo/images'
+labels_dir = './yolo/labels'
+images_output_dir = './voc/images'
+labels_output_dir = './voc/labels'
+
+for filename in os.listdir(images_dir):
+    if filename.endswith(".png") or filename.endswith(".jpg"):
+        img = Image.open(os.path.join(images_dir, filename))
+        img = img.convert('RGB')
+        img.save(os.path.join(images_output_dir, filename.split('.')[0] + '.jpeg'), 'JPEG', quality=100)
+    else:
+        if filename.filename.endswith(".jpeg"):
+            shutil.copy(os.path.join(images_dir, filename), images_output_dir)
+
+Convertor.yolo2voc(
+    dataset_dir=os.path.join(images_dir, '..'),
+    yolo_ann_dir=os.path.join(labels_dir, '..'),
+    save_dir=labels_output_dir
+)
--- dataset/sample.py
+++ dataset/sample.py
@@ -4,9 +4,11 @@ import os
 venv_dir = os.environ['VIRTUAL_ENV']
 os.chdir(venv_dir + '/../dataset')
 
-Sample.show_samples(data_path='./yolo/images',
-                    ann_path='./csv/dataset.csv',
-                    num_of_samples=5,
-                    ann_type='csv',
-                    seed=0,
-                    image_shape=[512, 512])
+Sample.show_samples(
+    data_path='./yolo/images',
+    ann_path='./voc/labels',
+    num_of_samples=5,
+    ann_type='voc',
+    seed=0,
+    image_shape=[512, 512]
+)
