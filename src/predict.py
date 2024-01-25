7386a888b27c35bbd1c5241b2152c376b131a9bf
z3lx <57017122+z3lx@users.noreply.github.com>
2024-01-25 12:03:55 -0500

    Refactor predict.py script

--- src/predict.py
+++ src/predict.py
@@ -1,24 +1,90 @@
+"""
+predict.py
+Predicts bounding boxes on images or videos.
+
+Usage:
+    python predict.py <model_path> <input_path> <threshold> <num_threads>
+
+    model_path (str): The path to the model.
+    input_path (str): The path to the input image or video.
+    threshold (float): The minimum confidence threshold.
+    num_threads (int): The number of threads to use.
+
+Example:
+    python predict.py runs/train_0/export/model.tflite dataset/voc/images_preprocessed/000000.jpeg 0.5 4
+"""
+
+import colorsys
 import os
 import cv2
 import tensorflow as tf
+import numpy as np
 from utils import create_subdirectory
-from utils import chdir_to_project_root
-from utils import run_odt_and_draw_results
+from utils import get_project_root
 
-DETECTION_THRESHOLD = 0.5
-RUNS_DIR = "runs"
+def infer(interpreter: tf.lite.Interpreter, image: np.ndarray, threshold: float) -> list:
+    # Preprocess the input image
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    tensor = tf.convert_to_tensor(image)
+    _, input_height, input_width, _ = interpreter.get_input_details()[0]["shape"]
+    input_size = (input_height, input_width)
+    tensor = tf.image.resize(tensor, input_size)
+    tensor = tensor[tf.newaxis, :]
+    tensor = tf.cast(tensor, dtype=tf.uint8)
 
-def process_image(interpreter, input_path, output_path):
+    # Run inference
+    signature_fn = interpreter.get_signature_runner()
+    output = signature_fn(images = tensor)
+
+    # Postprocess the output
+    count = int(np.squeeze(output["output_0"]))
+    scores = np.squeeze(output["output_1"])
+    classes = np.squeeze(output["output_2"])
+    boxes = np.squeeze(output["output_3"])
+
+    results = []
+    for i in range(count):
+        if scores[i] >= threshold:
+            result = {
+                "bounding_box": boxes[i],
+                "class_id": classes[i],
+                "score": scores[i]
+            }
+            results.append(result)
+    return results
+ 
+def draw_results(image: np.ndarray, results:list) -> None:
+    for obj in results:
+        # Convert bounding box from relative to absolute coordinates
+        ymin, xmin, ymax, xmax = obj["bounding_box"]
+        xmin = int(xmin * image.shape[1])
+        xmax = int(xmax * image.shape[1])
+        ymin = int(ymin * image.shape[0])
+        ymax = int(ymax * image.shape[0])
+
+        # Get class id
+        class_id = int(obj["class_id"])
+
+        # Generate color for the object class
+        hue = (class_id * 0.618033988749895) % 1.0
+        rgb_color = tuple(int(c * 255) for c in colorsys.hsv_to_rgb(hue, 1, 1))
+        color = rgb_color[::-1]
+
+        # Draw bounding box
+        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
+
+        # Draw label
+        y = ymin - 15 if ymin - 15 > 15 else ymin + 15
+        label = "id {}: {:.0f}%".format(class_id, obj["score"] * 100)
+        cv2.putText(image, label, (xmin, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+def process_image(interpreter: tf.lite.Interpreter, input_path: str, output_path: str, threshold: float) -> None:
     mat = cv2.imread(input_path)
-    output_img = run_odt_and_draw_results(
-        mat,
-        interpreter,
-        threshold = DETECTION_THRESHOLD
-    )
+    results = infer(interpreter, mat, threshold)
+    draw_results(mat, results)
+    cv2.imwrite(output_path, mat)
 
-    cv2.imwrite(output_path, output_img)
-
-def process_video(interpreter, input_path, output_path):
+def process_video(interpreter: tf.lite.Interpreter, input_path: str, output_path: str, threshold: float) -> None:
     cap = cv2.VideoCapture(input_path)
     out = cv2.VideoWriter(
         output_path,
@@ -28,24 +94,21 @@ def process_video(interpreter, input_path, output_path):
     )
 
     while(cap.isOpened()):
-        ret, frame = cap.read()
+        ret, mat = cap.read()
         if ret:
-            output_frame = run_odt_and_draw_results(
-                frame,
-                interpreter,
-                threshold = DETECTION_THRESHOLD
-            )
-            out.write(output_frame)
+            results = infer(interpreter, mat, threshold)
+            draw_results(mat, results)
+            out.write(mat)
         else:
             break
 
     cap.release()
     out.release()
 
-def main(model_path, input_path, num_threads = 8):
+def main(model_path: str, input_path: str, threshold: float, num_threads: int) -> None:
     # Create directory
-    chdir_to_project_root()
-    predict_dir = create_subdirectory(RUNS_DIR, "predict")
+    runs_dir = get_project_root("runs")
+    predict_dir = create_subdirectory(runs_dir, "predict")
     output_path = os.path.join(predict_dir, os.path.basename(input_path))
 
     # Load model
@@ -53,14 +116,20 @@ def main(model_path, input_path, num_threads = 8):
     interpreter.allocate_tensors()
 
     # Check file extension
-    ext = os.path.splitext(input_path)[1]
-    if ext in [".jpg", ".png", ".jpeg"]:
-        process_image(interpreter, input_path, output_path)
-    elif ext in [".mp4", ".avi"]:
-        process_video(interpreter, input_path, output_path)
+    ext = input_path.split(".")[-1]
+    if ext in ["jpg", "png", "jpeg"]:
+        process_image(interpreter, input_path, output_path, threshold)
+    elif ext in ["mp4", "avi"]:
+        process_video(interpreter, input_path, output_path, threshold)
     else:
         raise Exception(f"Unsupported file type: {ext}")
 
 if __name__ == "__main__":
     import sys
-    main(sys.argv[1], sys.argv[2])
+    try:
+        main(sys.argv[1], sys.argv[2], float(sys.argv[3]), int(sys.argv[4]))
+    except Exception:
+        import traceback
+        traceback.print_exc()
+        print(__doc__)
+        sys.exit(1)
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,4 @@
-import colorsys
 import os
-
-import cv2
-import numpy as np
-import tensorflow as tf
 
 def create_subdirectory(base_dir: str, prefix: str) -> str:
     """Create a subdirectory with a given prefix in the given base directory"""
@@ -16,91 +11,9 @@ def create_subdirectory(base_dir: str, prefix: str) -> str:
             break
     return sub_dir
 
-def chdir_to_project_root():
-    """Change the current working directory to the project root"""
+def get_project_root(relative_path: str = None) -> str:
+    """Get the root directory of the project"""
     venv_dir = os.environ["VIRTUAL_ENV"]
-    os.chdir(os.path.join(venv_dir, ".."))
-
-def preprocess_image(mat, input_size):
-    """Preprocess the input image to feed to the TFLite model"""
-    mat = cv2.cvtColor(mat, cv2.COLOR_BGR2RGB)
-    img = tf.convert_to_tensor(mat)
-    #img = tf.cast(img, dtype=tf.uint8)
-    original_image = img
-    resized_img = tf.image.resize(img, input_size)
-    resized_img = resized_img[tf.newaxis, :]
-    resized_img = tf.cast(resized_img, dtype=tf.uint8)
-    return resized_img, original_image
-
-def detect_objects(interpreter, image, threshold):
-    """Returns a list of detection results, each a dictionary of object info."""
-
-    signature_fn = interpreter.get_signature_runner()
-
-    # Feed the input image to the model
-    output = signature_fn(images=image)
-
-    # Get all outputs from the model
-    count = int(np.squeeze(output["output_0"]))
-    scores = np.squeeze(output["output_1"])
-    classes = np.squeeze(output["output_2"])
-    boxes = np.squeeze(output["output_3"])
-
-    results = []
-    for i in range(count):
-        if scores[i] >= threshold:
-            result = {
-                "bounding_box": boxes[i],
-                "class_id": classes[i],
-                "score": scores[i]
-            }
-            results.append(result)
-    return results
-
-def get_color(index):
-    """Generate a color for a given object class"""
-    hue = (index * 0.618033988749895) % 1.0
-    rgb_color = tuple(int(c * 255) for c in colorsys.hsv_to_rgb(hue, 1, 1))
-    return rgb_color
-
-def run_odt_and_draw_results(mat, interpreter, threshold = 0.5):
-    """Run object detection on the input image and draw the detection results"""
-    # Load the input shape required by the model
-    _, input_height, input_width, _ = interpreter.get_input_details()[0]["shape"]
-
-    # Load the input image and preprocess it
-    preprocessed_image, original_image = preprocess_image(
-        mat,
-        (input_height, input_width)
-    )
-
-    # Run object detection on the input image
-    results = detect_objects(interpreter, preprocessed_image, threshold=threshold)
-
-    # Plot the detection results on the input image
-    original_image_np = original_image.numpy().astype(np.uint8)
-
-    for obj in results:
-        # Convert the object bounding box from relative coordinates to absolute
-        # coordinates based on the original image resolution
-        ymin, xmin, ymax, xmax = obj["bounding_box"]
-        xmin = int(xmin * original_image_np.shape[1])
-        xmax = int(xmax * original_image_np.shape[1])
-        ymin = int(ymin * original_image_np.shape[0])
-        ymax = int(ymax * original_image_np.shape[0])
-
-        # Find the class index of the current object
-        class_id = int(obj["class_id"])
-
-        # Draw the bounding box and label on the image
-        color = get_color(class_id)
-        cv2.rectangle(original_image_np, (xmin, ymin), (xmax, ymax), color, 2)
-        # Make adjustments to make the label visible for all objects
-        y = ymin - 15 if ymin - 15 > 15 else ymin + 15
-        label = "id {}: {:.0f}%".format(class_id, obj["score"] * 100)
-        cv2.putText(original_image_np, label, (xmin, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-    # Return the final image
-    original_uint8 = original_image_np.astype(np.uint8)
-    original_uint8 = cv2.cvtColor(original_uint8, cv2.COLOR_RGB2BGR)
-    return original_uint8
+    relative_dir = os.path.join(venv_dir, "..", relative_path)
+    os.makedirs(relative_dir, exist_ok = True)
+    return relative_dir
