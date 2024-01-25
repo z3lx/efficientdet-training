a2a8fd9b4ac46c2808737ac4937fe9ee39670d80
z3lx <57017122+z3lx@users.noreply.github.com>
2024-01-25 12:56:48 -0500

    Refactor train.py script

--- src/train.py
+++ src/train.py
@@ -1,4 +1,20 @@
+"""
+train.py
+Trains an EfficientDet model on a dataset.
+
+Usage:
+    python train.py <dataset_annotation_dir> <dataset_image_dir> <debug>
+
+    dataset_annotation_dir (str): The directory where the dataset annotations are stored.
+    dataset_image_dir (str): The directory where the dataset images are stored.
+    debug (bool): Whether to run in debug mode.
+
+Example:
+    python train.py dataset/voc/labels dataset/voc/images True
+"""
+
 import os
+import time
 import tensorflow as tf
 from tflite_model_maker import model_spec
 from tflite_model_maker import object_detector
@@ -7,53 +23,71 @@ from tflite_model_maker.config import ExportFormat
 from utils import create_indexed_subdirectory
 from utils import get_project_root
 
-tf.get_logger().setLevel("INFO")
-from absl import logging
-logging.set_verbosity(logging.INFO)
+def main(dataset_annotation_dir:str, dataset_image_dir: str, debug: bool = False):
+    start_time = time.time()
 
-#print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
-#tf.debugging.set_log_device_placement(True)
+    from absl import logging
+    if debug:
+        tf.get_logger().setLevel("DEBUG")
+        logging.set_verbosity(logging.DEBUG)
 
-# Paths
-dataset_annotation_dir = "dataset/voc/labels"
-dataset_image_dir = "dataset/voc/images"
+        print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
+        tf.debugging.set_log_device_placement(True)
+    else:
+        tf.get_logger().setLevel("INFO")
+        logging.set_verbosity(logging.INFO)
 
-# Create directories
-runs_dir = get_project_root("runs")
-run_dir = create_indexed_subdirectory(runs_dir, "train")
-export_dir = os.path.join(run_dir, "export")
-os.mkdir(export_dir)
+    # Create directories
+    runs_dir = get_project_root("runs")
+    run_dir = create_indexed_subdirectory(runs_dir, "train")
+    export_dir = os.path.join(run_dir, "export")
+    os.mkdir(export_dir)
 
-# Set model architecture
-spec = model_spec.get(
-    "efficientdet_lite0",
-    model_dir = run_dir,
-    tf_random_seed = 111111
-)
+    # Set model architecture
+    spec = model_spec.get(
+        "efficientdet_lite0",
+        model_dir = run_dir,
+        tf_random_seed = 111111
+    )
 
-# Load dataset
-train_data = object_detector.DataLoader.from_pascal_voc(
-    annotations_dir = dataset_annotation_dir,
-    images_dir = dataset_image_dir,
-    label_map = ["0"]
-)
+    # Load dataset
+    train_data = object_detector.DataLoader.from_pascal_voc(
+        annotations_dir = dataset_annotation_dir,
+        images_dir = dataset_image_dir,
+        label_map = ["0"]
+    )
 
-# Train model
-model = object_detector.create(
-    train_data = train_data,
-    model_spec = spec,
-    batch_size = 32,
-    train_whole_model = True,
-    validation_data = train_data,
-    epochs = 1
-)
+    # Train model
+    model = object_detector.create(
+        train_data = train_data,
+        model_spec = spec,
+        batch_size = 32,
+        train_whole_model = True,
+        validation_data = train_data,
+        epochs = 1
+    )
 
-# Export model
-model.export(
-    export_dir = export_dir,
-    export_format = [
-        ExportFormat.TFLITE,
-        ExportFormat.SAVED_MODEL,
-        ExportFormat.LABEL
-    ]
-)
+    # Export model
+    model.export(
+        export_dir = export_dir,
+        export_format = [
+            ExportFormat.TFLITE,
+            ExportFormat.SAVED_MODEL,
+            ExportFormat.LABEL
+        ]
+    )
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print("Training took {:.2f} hours".format(elapsed_time / 3600))
+    print("Model exported to {}".format(export_dir))
+
+if __name__ == "__main__":
+    import sys;
+    try:
+        main(sys.argv[1], sys.argv[2], bool(sys.argv[3]))
+    except Exception:
+        import traceback
+        traceback.print_exc()
+        print(__doc__)
+        sys.exit(1)
